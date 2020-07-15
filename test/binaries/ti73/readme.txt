a120d979373b95ef5d9c6f633abbc2bc722ee001
Timendus <mail@timendus.com>
2020-07-15 23:32:16 +0200

    Add more tests

---
+++ test/binaries/ti73/readme.txt
@@ -0,0 +1,100 @@
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~BOGGLE~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+           by Joseph Binney
+
+For 2 to 6 Players
+Ages 8 to Adult
+
+Set-up: 
+	Send group file Boggle.73g to calculator, which contains
+	Boggle.73p, Boggl.73l, and ASCII.73l
+
+Object:
+	To list, within 3 minutes, as many words of the highest point
+	value as you can find among the random assortment of letters
+	in the calculator grid.
+
+Preparation:
+	Make sure each player has a pencil and paper ready.
+	
+Playing:
+	Run the BOGGLE program from the program menu.  After the screen
+	with the grid is done displaying and the players are ready, 
+	press enter to start the timer and place letters in the
+	squares.When the timer starts, each player searches the
+	assortment of letters for words of FOUR LETTERS OR MORE.  When
+	you find a word, write it down.
+	
+	Words are formed from ADJOINING LETTERS.  Letters must join in
+	the PROPER SEQUENCE to spell a word.  They may join
+	horizontally, vertically or diagonally to the left, right or
+	up-and-don.  No letter square, however, may be used more than
+	once within a single word.
+
+	The only words that are allowed are those that can be found in
+	a standard English dictionary.  You may look for any type of
+	word-noun, verb, adjective, adverb, etc.  Plural nouns are
+	acceptable, as are all verb tenses.  Words within words are
+	allowed; e.g., MASTER: mast, aster.
+
+	Proper nouns, abbreviations, contractions, hyphenated words and
+	foreign words that are not in an English dictionary are not
+	allowed.
+
+Scoring:
+	When the timer runs out (or if finished early, press enter), 
+	everyone must stop writing.  Each player in turn then reads
+	aloud his or her list of words.  Any word that appears on more 
+	than one player's list must be crossed off ALL lists, including
+	that of the reader.  After all players have read their lists, 
+	each player scores his or her remaining words:
+
+# OF LETTERS:      4    5    6    7    8 or more
+-------------------------------------------------
+POINTS:            1    2    3    5    11
+
+
+	
+	The winner is: a) the player whose words have earned the most
+	points in a single round; or b) the first to reach 50 points, 
+	100 points or whatever score is considered by all to be a 
+	reasonable target in a series of rounds.  Once finished with a
+	single round, ONE player may enter his or her score into the
+	calculator, which will keep score through the game for this
+	player.  Once all desired rounds have been completed, you will
+	be prompted whether you want to play another game.  If yes is
+	selected, then a different game will begin, clearing the score.
+
+Things to Remember:
+	*Multiple meanings of the same word do not earn multiple
+	 credit.  The word "liner," for example, can mean a "line
+	 drive," "a protective inner covering," or "an ocean-going
+	 vessel"; yet it may count as only one word.
+	
+	*The same word found by a player in different areas of the
+	 grid may not be counted for multiple credit.
+
+	*Full credit is awarded for BOTH the singular and plural forms
+	 of a noun-but ONLY if the player has listed them as separate
+	 words.
+
+Optional Game Play:
+	Advanced Play: Each player must look only for words of 5
+		       letters or more.
+	
+	Handicap Play: While older or more experienced players look for
+		       for words of 4 letters or more, younger or less
+		       experienced players look for words of only 3
+		       letters or more.  (Score 1 point for each 3-
+		       letter word.)
+
+	Team Play: Each player on a team writes down his or her own
+		   words.  If teammates find the same word-a word that
+		   is NOT found by the opposition-only one team member
+		   crosses it off.  The winning team is the one with
+		   the most points.  If the players aren't of equal
+		   ability or experience, try matching a team of
+		   average players against an advanced/beginner team.
+		   Or put a team of younger or less experienced players
+		   agains one player who is older of more experienced.
+
+~ENJOY!~ :)
--- test/tifiles.test.js
+++ test/tifiles.test.js
@@ -3,21 +3,74 @@ const fs = require('fs');
 
 describe('tifiles.js', () => {
 
-  describe('parsing TI-84 Plus files', () => {
+  describe('isValid', () => {
+    it('returns true for a file with a calc type, sane size and some entries', () => {
+      expect(tifiles.isValid({
+        calcType: 'TI-84 Plus',
+        size: 315,
+        debug: {
+          sizeCorrect: true
+        },
+        entries: [ {} ]
+      })).toBe(true);
+    });
+    it('returns false for a file without a calcType', () => {
+      expect(tifiles.isValid({
+        calcType: 'NONE',
+        size: 315,
+        debug: {
+          sizeCorrect: true
+        },
+        entries: [ {} ]
+      })).toBe(false);
+    });
+    it('returns false for a file without a size of zero', () => {
+      expect(tifiles.isValid({
+        calcType: 'TI-84 Plus',
+        size: 0,
+        debug: {
+          sizeCorrect: true
+        },
+        entries: [ {} ]
+      })).toBe(false);
+    });
+    it('returns false for a file with a wrong size (no checksum present, for example)', () => {
+      expect(tifiles.isValid({
+        calcType: 'TI-84 Plus',
+        size: 315,
+        debug: {
+          sizeCorrect: false
+        },
+        entries: [ {} ]
+      })).toBe(false);
+    });
+    it('returns false for a file with no entries', () => {
+      expect(tifiles.isValid({
+        calcType: 'TI-84 Plus',
+        size: 315,
+        debug: {
+          sizeCorrect: true
+        },
+        entries: []
+      })).toBe(false);
+    });
+  });
+
+  describe('parsing TI-73 files', () => {
 
     it('can parse a program file', () => {
-      const file = read('./test/binaries/ti84p/romdump.8Xp');
+      const file = read('./test/binaries/ti73/romdump.73p');
       expectEntriesToAddUpToFileSize(file);
       expect(file.entries.length).toBe(1);
       expect(file).toMatchObject({
-        calcType: 'TI-84 Plus',
-        comments: 'ProtProgram file 04/06/05, 16:33',
-        size: 438,
+        calcType: 'TI-73',
+        comments: 'Created by ticonv 0.1, 07/28/2005',
+        size: 731,
         entries: [
           {
             name: 'ROMDUMP',
             type: 6,
-            size: 421
+            size: 716
           }
         ],
         debug: {
@@ -27,13 +80,69 @@ describe('tifiles.js', () => {
     });
 
     it('can parse a group file', () => {
-      const file = read('./test/binaries/ti84p/group.8Xg');
+      const file = read('./test/binaries/ti73/Boggle.73g');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(3);
+      expect(file).toMatchObject({
+        calcType: 'TI-73',
+        comments: '05/29/02, 12:47',
+        size: 1451,
+        entries: [
+          {
+            name: 'BOGGLE',
+            type: 5,
+            size: 1159
+          },
+          {
+            name: ']BOGGL',
+            type: 1,
+            size: 11
+          },
+          {
+            name: ']ASCII',
+            type: 13,
+            size: 236
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+  });
+
+  describe('parsing TI-82 files', () => {
+
+    it('can parse a program file', () => {
+      const file = read('./test/binaries/ti82/math.82p');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(1);
+      expect(file).toMatchObject({
+        calcType: 'TI-82',
+        comments: 'File received by TiLP',
+        size: 116,
+        entries: [
+          {
+            name: 'TVA',  // Not sure if this is right
+            type: 5,      // But it could be ;)
+            size: 101
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+    it('can parse a group file', () => {
+      const file = read('./test/binaries/ti82/group.82g');
       expectEntriesToAddUpToFileSize(file);
       expect(file.entries.length).toBe(2);
       expect(file).toMatchObject({
-        calcType: 'TI-84 Plus',
-        comments: 'Real file 04/06/05, 16:32',
-        size: 52,
+        calcType: 'TI-82',
+        comments: 'File received by TiLP',
+        size: 48,
         entries: [
           {
             name: 'A',
@@ -95,6 +204,159 @@ describe('tifiles.js', () => {
             name: 'B',
             type: 0,
             size: 9
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+  });
+
+  describe('parsing TI-84 Plus files', () => {
+
+    it('can parse a program file', () => {
+      const file = read('./test/binaries/ti84p/romdump.8Xp');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(1);
+      expect(file).toMatchObject({
+        calcType: 'TI-84 Plus',
+        comments: 'ProtProgram file 04/06/05, 16:33',
+        size: 438,
+        entries: [
+          {
+            name: 'ROMDUMP',
+            type: 6,
+            size: 421
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+    it('can parse a group file', () => {
+      const file = read('./test/binaries/ti84p/group.8Xg');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(2);
+      expect(file).toMatchObject({
+        calcType: 'TI-84 Plus',
+        comments: 'Real file 04/06/05, 16:32',
+        size: 52,
+        entries: [
+          {
+            name: 'A',
+            type: 0,
+            size: 9
+          },
+          {
+            name: 'B',
+            type: 0,
+            size: 9
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+  });
+
+  describe('parsing TI-85 files', () => {
+
+    it('can parse a program file', () => {
+      const file = read('./test/binaries/ti85/MATH.85P');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(1);
+      expect(file).toMatchObject({
+        calcType: 'TI-85',
+        comments: 'Program file dated 12/27/97, 08:31',
+        size: 410,
+        entries: [
+          {
+            name: 'FList',
+            type: 18,
+            size: 397
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+    it('can parse a group file', () => {
+      const file = read('./test/binaries/ti85/GROUP.85G');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(2);
+      expect(file).toMatchObject({
+        calcType: 'TI-85',
+        comments: 'Group file dated 07/21/05, 15:45',
+        size: 38,
+        entries: [
+          {
+            name: 'A',
+            type: 0,
+            size: 10
+          },
+          {
+            name: 'B',
+            type: 0,
+            size: 10
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+  });
+
+  describe('parsing TI-86 files', () => {
+
+    it('can parse a program file', () => {
+      const file = read('./test/binaries/ti86/prgm.86p');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(1);
+      expect(file).toMatchObject({
+        calcType: 'TI-86',
+        comments: 'Created by ticonv 0.1, 07/28/2005',
+        size: 707,
+        entries: [
+          {
+            name: 'ROMDump',
+            type: 18,
+            size: 692
+          }
+        ],
+        debug: {
+          sizeCorrect: true
+        }
+      });
+    });
+
+    it('can parse a group file', () => {
+      const file = read('./test/binaries/ti86/group.86g');
+      expectEntriesToAddUpToFileSize(file);
+      expect(file.entries.length).toBe(2);
+      expect(file).toMatchObject({
+        calcType: 'TI-86',
+        comments: 'File received by tilp',
+        size: 52,
+        entries: [
+          {
+            name: 'Y',
+            type: 0,
+            size: 10
+          },
+          {
+            name: 'X',
+            type: 0,
+            size: 10
           }
         ],
         debug: {
