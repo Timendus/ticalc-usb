ed0ce02a1a7b0fe8645e204361573641d588053d
commandblockguy <bluestemjc@gmail.com>
2021-09-16 22:49:36 -0500

    Add tests for mergeBuffers

--- test/byte-mangling.test.js
+++ test/byte-mangling.test.js
@@ -228,4 +228,28 @@ describe('byte-mangling.js', () => {
     });
   });
 
+  describe('mergeBuffers', () => {
+    it('combines multiple arrays', () => {
+      expect(b.mergeBuffers(
+        [new Uint8Array([1,2,3]), new Uint8Array([4,5,6]), new Uint8Array([7,8,9])]
+      )).toEqual(
+        new Uint8Array([1,2,3,4,5,6,7,8,9])
+      );
+    });
+    it('handles empty arrays', () => {
+      expect(b.mergeBuffers(
+        [new Uint8Array([1,2,3]), new Uint8Array([]), new Uint8Array([7,8,9])]
+      )).toEqual(
+        new Uint8Array([1,2,3,7,8,9])
+      );
+    });
+    it('preserves a single array', () => {
+      expect(b.mergeBuffers(
+        [new Uint8Array([1,2,3])]
+      )).toEqual(
+        new Uint8Array([1,2,3])
+      );
+    });
+  });
+
 });
